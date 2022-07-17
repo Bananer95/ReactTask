0ce49eb5a32555f6ae6367afd2f38946767de0fa
Bananer95 <Bananer95@mail.ru>
2022-07-17 13:45:13 +0200

    feat: add links in posts

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import 'jquery';
+import 'bootstrap/dist/js/bootstrap';
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+import './assets/css/bootstrap.min.css';
+import './assets/css/mediumish.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
--- src/pages/main/sections/Featured.tsx
+++ src/pages/main/sections/Featured.tsx
@@ -1,33 +1,34 @@
-import React from "react";
-import { ArticleCardRow, CardColumns } from "../../../components/elements/Card";
+import React from 'react';
+import { ArticleCardRow, CardColumns } from '../../../components/elements/Card';
 
-import { SectionTitle } from "../../../components/sections/common";
+import { SectionTitle } from '../../../components/sections/common';
 
 //mock data
-import { featuredArticles } from "./articlesMockData";
+import { featuredArticles } from './articlesMockData';
+import { navigation } from '../../../layouts/components/header/Header';
 
 const FeaturedSection = () => {
-	return (
-		<section className="featured-posts">
-			<SectionTitle>Featured</SectionTitle>
-			<CardColumns className="listfeaturedtag">
-				{featuredArticles.map((article) => (
-					<ArticleCardRow
-						key={article.id}
-						link={article.link}
-						thumbUrl={article.thumbUrl}
-						title={article.title}
-						text={article.text}
-						postDate={article.postDate}
-						postReadTime={article.postReadTime}
-						authorName={article.authorName}
-						authorLink={article.authorLink}
-						authorAvatarUrl={article.authorAvatarUrl}
-					/>
-				))}
-			</CardColumns>
-		</section>
-	);
+  return (
+    <section className="featured-posts">
+      <SectionTitle>Featured</SectionTitle>
+      <CardColumns className="listfeaturedtag">
+        {featuredArticles.map((article) => (
+          <ArticleCardRow
+            key={article.id}
+            link={article.link}
+            thumbUrl={article.thumbUrl}
+            title={article.title}
+            text={article.text}
+            postDate={article.postDate}
+            postReadTime={article.postReadTime}
+            authorName={article.authorName}
+            authorLink={article.authorLink}
+            authorAvatarUrl={article.authorAvatarUrl}
+          />
+        ))}
+      </CardColumns>
+    </section>
+  );
 };
 
 export default FeaturedSection;
